ac77f327f1a156a8a2074341b2187088bbbf5c86
yanndarkz <iansantosmcqueen@gmail.com>
2024-12-04 22:10:05 -0300

    feat: update app.routes

--- Hackton-restic36/src/app/app.routes.ts
+++ Hackton-restic36/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
-import { WindowAplicationComponent } from './views/window-aplication/window-aplication.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { LoginComponent } from './Pages/login/login.component';
 
 
 export const routes: Routes = [
-    {path:'', component: WindowAplicationComponent}
+    {path:'', component: LoginComponent},
+    {path:'home', component: HomeComponent}
 ];
