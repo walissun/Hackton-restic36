0c26133ce668edd5c289e2699a238ebb53b20dd3
yanndarkz <iansantosmcqueen@gmail.com>
2024-12-07 06:41:41 -0300

    feature: config angular

--- Frontend-angular/src/app/app.config.ts
+++ Frontend-angular/src/app/app.config.ts
@@ -1,11 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(), { provide: FormsModule },]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch()), { provide: FormsModule },]
 };
