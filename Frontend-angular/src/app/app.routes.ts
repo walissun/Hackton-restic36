b2e36bcb654c77b15ecd82160f08abb5ca97dd76
yanndarkz <iansantosmcqueen@gmail.com>
2024-12-08 13:07:31 -0300

    feature: configApi map

--- Frontend-angular/src/app/app.routes.ts
+++ Frontend-angular/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
-import { HomeComponent } from './Pages/home/home.component';
 import { LoginComponent } from './Pages/login/login.component';
+import { MapaComponent } from './componentes/mapa/mapa.component';
 
 
 export const routes: Routes = [
     {path:'', component: LoginComponent},
-    {path:'home', component: HomeComponent}
+    {path:'home', component: MapaComponent},
+    { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
